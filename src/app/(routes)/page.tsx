f46aeb8a3b2e80d54440975154beb2e5b15fe758
jastrzembowski <jastrzembowski@users.noreply.github.com>
2026-01-06 15:20:46 +0100

    fix

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -5,6 +5,9 @@ import { MainPage } from "./MainPage";
 export default async function Home() {
   const days = await getAllDays();
   
+  if (!days) {
+    return <div>Coś poszło nie tak</div>;
+  }
   return (
     <MainPage days={days}/>
   );
